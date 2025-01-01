f7aef832a14a0770eaf361017dd981cf40011e16
masundi1 <manjunathasundi07@gmail.com>
2025-01-01 23:33:22 +0530

    ML AI notes added

---
+++ ML-AI Notes.dart
@@ -0,0 +1,37 @@
+
+- AI is making the system as intelligent as human as.
+
+
+- ML is subset of AI
+- How ML works? 
+    - Use existing data to train the model
+    - Build model
+    - Use model to predict the output
+
+- Input == Features - Required to predict the output
+- Ouput == Label - Based on features, predicting the label with the help of model 
+
+- Example Models:
+    - House pricing prediction model(Regression) :
+        - Label: Price
+        - Features: area, rooms, kitchen, balcony and near by places
+
+    - User car pricing model(Regression)
+    - Spam email model(Classision)
+    - Provide loan model (Classision)
+
+- Prediction possibilities of Label
+    - Regression Model: If label is a number with a range
+    - Classision: If label fall under set of predefined values.
+
+
+- Creating ML Models
+    - Obtain data
+    - Clean data
+    - Feature Engineering: Find all Features and Label
+    - Create model with ML algorithm
+    - Evaluate the accuracy of the prediction
+    - Deploy the model
+
+
+
